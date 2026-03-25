9f2d87a75182d7278c79914f8717e65c9a5d9150
freewms <36444868+freewms@users.noreply.github.com>
2026-03-25 06:58:46 +0300

    Change return value to UART_FLUSH_RESULT_SUCCESS

--- components/ble_nus_client/ble_nus_client.cpp
+++ components/ble_nus_client/ble_nus_client.cpp
@@ -210,7 +210,7 @@ uart::FlushResult BLENUSClientComponent::flush() {
       break;
     }
     //delay(5);
-    return uart::FlushResult::UART_FLUSH_RESULT_SUCCESS;
+    return uart::FlushResult::SUCCESS;
   }
 }
 
